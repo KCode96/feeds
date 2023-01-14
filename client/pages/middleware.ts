a492c467feb45445e0077ef3b471e88d8de375bd
moonGoblin <kaungzaw1996@gmail.com>
2023-01-14 15:05:30 +1100

    deleted middleware.tx file in client

--- client/pages/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-import { NextServer } from 'next/dist/server/next';
-
-export function middleware(req: NextRequest) {
-    console.log('Hello World NEXT!');
-    return NextResponse.next();
-}
